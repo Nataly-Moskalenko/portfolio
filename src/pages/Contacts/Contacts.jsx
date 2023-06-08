02b6c49dd6151fa5e7475cea1f2c75ac56e1a66e
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-08 20:09:00 +0300

    adds react-icons

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,45 +1,57 @@
 import css from './Contacts.module.css';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
 
 export default function Contacts() {
   return (
     <div className={css.contacts}>
-      <h1 className={css.contactsTitle}>My contacts:</h1>
+      <h1 className={css.contactsTitle}>My contacts</h1>
+      <div className={css.contactsLink}>
+        <FaEnvelope />
+        <span>nmoskalenko911@gmail.com</span>
+      </div>
+
       {/* <p>
         <a href="mailto:nmoskalenko911@gmail.com">Email</a>
       </p> */}
       {/* <p>Call me: </p>
       <p>Email: nmoskalenko911@gmail.com</p>
       <p>Location: Sumy, Ukraine</p> */}
-      <p>
+      <div>
         <a
           className={css.contactsLink}
           href="https://t.me/tasha8"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Telegram
+          <FaTelegramPlane />
+          <span>Telegram</span>
         </a>
-      </p>
-      <p>
+      </div>
+      <div>
         <a
           className={css.contactsLink}
           href="https://www.linkedin.com/in/nataly-moskalenko/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          LinkedIn
+          <FaLinkedinIn />
+          <span>LinkedIn</span>
         </a>
-      </p>
-      <p>
+      </div>
+      <div>
         <a
           className={css.contactsLink}
           href="https://github.com/Nataly-Moskalenko"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          <FaGithub />
+          <span>Github</span>
         </a>
-      </p>
+      </div>
     </div>
   );
 }
