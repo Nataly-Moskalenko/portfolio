88a6fc2f917789096bc01cd82df3df866797c205
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-08 14:59:43 +0300

    adds slider and script for 404

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,12 +1,34 @@
+import css from './Contacts.module.css';
+
 export default function Contacts() {
   return (
-    <div>
+    <div className={css.contacts}>
       <h1>My contacts:</h1>
-      {/* <p>Email: nmoskalenko911@gmail.com</p> */}
-      <p>Telegram</p>
-      <p>LinkedIn</p>
-      <p>Github</p>
-      {/* <p>Sumy, Ukraine</p> */}
+      {/* <p>
+        <a href="mailto:nmoskalenko911@gmail.com">Email</a>
+      </p> */}
+      {/* <p>Call me: </p>
+      <p>Email: nmoskalenko911@gmail.com</p>
+      <p>Location: Sumy, Ukraine</p> */}
+      <p>
+        <a href="https://t.me/tasha8" target="_blank" rel="noopener noreferrer">
+          Telegram
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/nataly-moskalenko/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/Nataly-Moskalenko" target="_blank" rel="noopener noreferrer">
+          Github
+        </a>
+      </p>
     </div>
   );
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,16 +1,28 @@
+import Slider from 'react-slick';
 import { projects } from '../../data';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import css from './Projects.module.css';
 
 export default function Projects() {
+  const settings = {
+    dots: true,   
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,    
+  };
   return (
-    <div>
-      <h1>My Projects</h1>
+    <div className={css.projects}>
+      <h1 className={css.projectsTitle}>My Projects</h1>
       <ul>
-        {projects.map((project) => (
-          <li key={project.id}>
-            <ProjectCard project={project} />
-          </li>
-        ))}
+        <Slider {...settings}>
+          {projects.map((project) => (
+            <li key={project.id}>
+              <ProjectCard project={project} />
+            </li>
+          ))}
+        </Slider>
       </ul>
     </div>
   );
