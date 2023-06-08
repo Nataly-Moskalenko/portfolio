544391a215e88d256c4ffbd8ad1acabaf23b37c7
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-08 12:36:45 +0300

    Updates

---
+++ static/js/969.761e1a8c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[969],{969:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});var i=r(184);function c(){return(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:"My contacts:"}),(0,i.jsx)("p",{children:"Telegram"}),(0,i.jsx)("p",{children:"LinkedIn"}),(0,i.jsx)("p",{children:"Github"})]})}}}]);
+//# sourceMappingURL=969.761e1a8c.chunk.js.map
